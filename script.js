60991977d1bbfa4f7fa80f850d149f2d25f61dce
jessica <jessicaelen21052000@gmail.com>
2019-04-16 11:28:30 -0300

    let java

---
+++ script.js
@@ -0,0 +1,23 @@
+let senhaCadastrada = "coalacoalacoala";
+let senhaEntrada = prompt("digite sua senha: ");
+let vlrSaqueDiario = 550;
+
+if(senhaCadastrada===senhaEntrada){
+let saldo = 600;
+let vlrSaque = prompt("Qual o valor do saque?");
+
+
+if((vlrSaque<=vlrSaqueDiario) && (vlrSaque<=vlrSaqueDiario)){
+    alert("saque permitido");
+}
+else{
+    alert("você exedeu seu limite  de saque diario");
+}
+
+else{
+    alert("Saldo insulficiente");
+}
+}
+else{
+    alert("senha incorreta");
+}
